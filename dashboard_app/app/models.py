7abacab7716d88b8b6928c42dc88d5cf4c7d5c2b
rowanv <rrvspam@gmail.com>
2016-02-03 14:47:41 +0100

    End of Section 4 - Creating a Dashboard Bar Chart Element.

--- dashboard_app/app/models.py
+++ dashboard_app/app/models.py
@@ -1,4 +1,5 @@
 from pandas.io.json import json_normalize
+import pandas as pd
 import requests
 
 class EarthquakeMonth:
@@ -6,7 +7,8 @@ class EarthquakeMonth:
     for all earthquakes on earth over the
     course of a mont.'''
     def __init__(self):
-        earthquake_month_url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
-        self.response = requests.get(earthquake_month_url)
-        self.df = json_normalize(self.response.json()['features'])
+        # earthquake_month_url = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
+        # self.response = requests.get(earthquake_month_url)
+        # self.df = json_normalize(self.response.json()['features'])
+        self.df = pd.read_csv('../earthquakes_data_Feb_2016.csv')
         self.max_mag_row = self.df.ix[self.df['properties.mag'].idxmax(),]
--- dashboard_app/app/views.py
+++ dashboard_app/app/views.py
@@ -1,6 +1,7 @@
 from app import app
 from .models import EarthquakeMonth
-from flask import render_template
+from flask import render_template, Markup
+from nvd3 import discreteBarChart
 
 
 @app.route('/')
@@ -9,8 +10,28 @@ def index():
     max_mag_row = earthquake_month.max_mag_row
     max_mag_place = max_mag_row['properties.place']
     max_mag_mag = max_mag_row['properties.mag']
+    bar_chart_alert_html = Markup(bar_chart_alert(
+        earthquake_month.df))
     context = {
         'max_mag_place': max_mag_place,
         'max_mag_mag': max_mag_mag,
+        'bar_chart_alert_html': bar_chart_alert_html,
     }
     return render_template('index.html', context=context)
+
+def bar_chart_alert(df):
+    chart_type = 'discreteBarChart'
+    chart = discreteBarChart(
+        name=chart_type,
+        height=200,
+        width=400,
+        jquery_on_ready=True)
+
+    ydata = [int(x) for x in df['properties.alert'].value_counts()]
+    xdata = df['properties.alert'].value_counts().index
+
+    chart.add_serie(y=ydata,x=xdata)
+    chart.buildhtml()
+
+    chart_html = chart.htmlcontent
+    return chart_html
