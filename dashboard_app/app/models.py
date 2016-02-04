5ad0f698aac9a2cbdf46e46b7ad1abf6dbba9d42
rowanv <rrvspam@gmail.com>
2016-02-04 18:20:45 +0100

    Added second bar CDI element. Refactored to use DashboardBarWidget model

--- dashboard_app/app/models.py
+++ dashboard_app/app/models.py
@@ -1,6 +1,8 @@
 from pandas.io.json import json_normalize
 import pandas as pd
 import requests
+from nvd3 import discreteBarChart
+from flask import Markup
 
 class EarthquakeMonth:
     '''Represents the observations gathered
@@ -12,3 +14,27 @@ class EarthquakeMonth:
         # self.df = json_normalize(self.response.json()['features'])
         self.df = pd.read_csv('../earthquakes_data_Feb_2016.csv')
         self.max_mag_row = self.df.ix[self.df['properties.mag'].idxmax(),]
+
+
+class DashboardBarWidget:
+
+    def __init__(self, chart_name, height, width,
+        xdata, ydata):
+        self.chart_name = chart_name
+        self.height = height
+        self.width = width
+        self.xdata = xdata
+        self.ydata = ydata
+
+    def get_html(self):
+        chart_type = self.chart_name
+        chart = discreteBarChart(
+            name=chart_type,
+            height=self.height,
+            width=self.width,
+            jquery_on_ready=True)
+        chart.add_serie(
+            y=self.ydata,
+            x=self.xdata)
+        chart.buildhtml()
+        return Markup(chart.htmlcontent)
--- dashboard_app/app/views.py
+++ dashboard_app/app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from .models import EarthquakeMonth
+from .models import EarthquakeMonth, DashboardBarWidget
 from flask import render_template, Markup
 from nvd3 import discreteBarChart
 
@@ -10,28 +10,33 @@ def index():
     max_mag_row = earthquake_month.max_mag_row
     max_mag_place = max_mag_row['properties.place']
     max_mag_mag = max_mag_row['properties.mag']
-    bar_chart_alert_html = Markup(bar_chart_alert(
-        earthquake_month.df))
+
+    df = earthquake_month.df
+
+    x_data_alert = [int(x) for x in
+        df['properties.alert'].value_counts()]
+    y_data_alert = df['properties.alert'].value_counts(
+        ).index
+
+    bar_chart_alert = DashboardBarWidget(
+        'bar_chart_alert', 400, 400, y_data_alert,
+        x_data_alert)
+    bar_chart_alert_html = bar_chart_alert.get_html()
+
+    rounded_properties_cdi = df['properties.cdi'].round()
+    cdi_value_counts = rounded_properties_cdi.value_counts()
+    y_data_cdi = [int(x) for x in cdi_value_counts]
+    x_data_cdi = [int(x) for x in cdi_value_counts.index]
+    bar_chart_cdi = DashboardBarWidget(
+        'bar_chart_cdi', 400, 400, x_data_cdi, y_data_cdi)
+    bar_chart_cdi_html = bar_chart_cdi.get_html()
+
+
+
     context = {
         'max_mag_place': max_mag_place,
         'max_mag_mag': max_mag_mag,
         'bar_chart_alert_html': bar_chart_alert_html,
+        'bar_chart_cdi_html': bar_chart_cdi_html,
     }
     return render_template('index.html', context=context)
-
-def bar_chart_alert(df):
-    chart_type = 'discreteBarChart'
-    chart = discreteBarChart(
-        name=chart_type,
-        height=200,
-        width=400,
-        jquery_on_ready=True)
-
-    ydata = [int(x) for x in df['properties.alert'].value_counts()]
-    xdata = df['properties.alert'].value_counts().index
-
-    chart.add_serie(y=ydata,x=xdata)
-    chart.buildhtml()
-
-    chart_html = chart.htmlcontent
-    return chart_html
